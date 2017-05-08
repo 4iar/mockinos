8ffeb7056869045cd0145166f1a1dc32650cc699
4iar <me@4iar.com>
2017-05-09 00:18:51 +0100

    Add placeholder tests

---
+++ server.spec.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+
+describe('server', function () {
+  it ('changes the status id appropriately', function (done) {
+    done();
+  });
+
+  it('GET /pizzaTracker/getTrackerConfig responds with two blank lines', function (done) {
+    done();
+  });
+
+  it('throws an error if the status id is not valid', function (done) {
+    done();
+  });
+});
