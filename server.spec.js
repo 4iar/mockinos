73e31e836a5f0e0f504994e1ee557287eca42823
4iar <me@4iar.com>
2017-05-09 00:49:15 +0100

    Add tests for getTrackerConfig route

--- server.spec.js
+++ server.spec.js
@@ -1,16 +1,31 @@
-const expect = require('chai').expect;
-const assert = require('chai').assert;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+const server = require('./server.js').app;
 
 describe('server', function () {
-  it ('changes the status id appropriately', function (done) {
-    done();
+  it('should change the status id appropriately', function (done) {
+   done();
   });
 
   it('GET /pizzaTracker/getTrackerConfig responds with two blank lines', function (done) {
-    done();
+    chai.request(server)
+      .get('/pizzaTracker/getTrackerConfig')
+      .end((err, res) => {
+        should.not.exist(err, 'should be no errors');
+
+        res.status.should.equal(200, 'should respond with status code 200');
+
+        res.text.should.equal('\n\n', 'should respond with two linebreaks');
+
+        done();
+      });
   });
 
-  it('throws an error if the status id is not valid', function (done) {
+  it('rejects an invalid status id', function (done) {
     done();
   });
 });
