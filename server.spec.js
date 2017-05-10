125bd57df0015ea90aed5ad8f74f395ba5a6f5a0
4iar <me@4iar.com>
2017-05-10 12:12:42 +0100

    Add tests for status id changer

--- server.spec.js
+++ server.spec.js
@@ -1,14 +1,41 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
+const async = require('async');
 
 chai.use(chaiHttp);
 
 const server = require('./server.js').app;
+const changeStatusId = require('./server.js').changeStatusId;
 
 describe('server', function () {
   it('should change the status id appropriately', function (done) {
-   done();
+    async.series([
+      function (cb) {
+        changeStatusId(2);
+        chai.request(server)
+          .get('/pizzaTracker/getOrderStatus')
+          .end((err, res) => {
+            should.not.exist(err, 'should be no errors');
+            res.status.should.equal(200, 'should respond with status code 200');
+            JSON.parse(res.text).statusId.should.equal(2, 'status id should change appropriately');
+            cb();
+          });
+      },
+      function (cb) {
+        changeStatusId(5);
+        chai.request(server)
+          .get('/pizzaTracker/getOrderStatus')
+          .end((err, res) => {
+            should.not.exist(err, 'should be no errors');
+            res.status.should.equal(200, 'should respond with status code 200');
+            JSON.parse(res.text).statusId.should.equal(5, 'status id should change appropriately');
+            cb();
+          });
+      },
+    ], function() {
+      done();
+    })
   });
 
   it('GET /pizzaTracker/getTrackerConfig responds with two blank lines', function (done) {
